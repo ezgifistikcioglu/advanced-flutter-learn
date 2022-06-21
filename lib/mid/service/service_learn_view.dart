c3f52e700df5e33adbb77fbe256ada87ecea9ee4
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2022-06-22 00:27:11 +0300

    Added basic fetchPostItems.

--- lib/mid/service/service_learn_view.dart
+++ lib/mid/service/service_learn_view.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_full/mid/service/post_model.dart';
 
@@ -10,16 +13,76 @@ class ServiceLearn extends StatefulWidget {
 
 class _ServiceLearnState extends State<ServiceLearn> {
   List<PostModel>? _items;
+  String? name;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    name = "unKnown";
+    fetchPostItems();
+  }
+
+  void changeLoading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  Future<void> fetchPostItems() async {
+    changeLoading();
+    final response =
+        await Dio().get('https://jsonplaceholder.typicode.com/posts');
+    if (response.statusCode == HttpStatus.ok) {
+      final _myDatas = response.data;
+
+      if (_myDatas is List) {
+        setState(() {
+          _items = _myDatas.map((e) => PostModel.fromJson(e)).toList();
+        });
+      }
+    }
+    changeLoading();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(name ?? ''),
+        actions: [
+          _isLoading
+              ? const CircularProgressIndicator.adaptive()
+              : const SizedBox.shrink()
+        ],
+      ),
       body: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
         itemCount: _items?.length ?? 0,
         itemBuilder: (context, index) {
-          return Text('data');
+          return _PostCard(model: _items?[index]);
         },
+      ),
+    );
+  }
+}
+
+class _PostCard extends StatelessWidget {
+  const _PostCard({
+    Key? key,
+    required PostModel? model,
+  })  : _model = model,
+        super(key: key);
+
+  final PostModel? _model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        title: Text(_model?.title ?? ''),
+        subtitle: Text(_model?.body ?? ''),
       ),
     );
   }
