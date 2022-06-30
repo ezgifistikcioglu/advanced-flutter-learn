592bd4df237d53346cd6d880dc270746861360af
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2022-06-30 12:34:56 +0300

    Completed provider usage.

--- lib/advance_mid/reqrest_resource/view/reqrest_view.dart
+++ lib/advance_mid/reqrest_resource/view/reqrest_view.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_full/advance_mid/reqrest_resource/service/reqrest_service.dart';
 import 'package:flutter_full/advance_mid/reqrest_resource/viewModel/reqres_provider.dart';
@@ -7,7 +5,7 @@ import 'package:flutter_full/product/extension/string_extension.dart';
 import 'package:flutter_full/product/service/project_dio.dart';
 import 'package:provider/provider.dart';
 
-import '../viewModel/reqrest_view_model.dart';
+import '../model/resource_model.dart';
 
 class ReqrestView extends StatefulWidget {
   const ReqrestView({Key? key}) : super(key: key);
@@ -28,19 +26,41 @@ class _ReqrestViewState extends State<ReqrestView> with ProjectDioMixin {
     return ChangeNotifierProvider<ReqresProvider>(
       builder: (context, child) {
         return Scaffold(
-            // appBar: AppBar(
-            //     title: isLoading ? const CircularProgressIndicator() : null),
-            // body: ListView.builder(
-            //     itemCount: resources.length,
-            //     itemBuilder: (BuildContext context, int index) {
-            //       //inspect(resources[index].colorValue);
-            //       return Card(
-            //           color: Color(resources[index].color?.colorValue ?? 0),
-            //           child: Text(resources[index].name ?? ''));
-            //     }),
-            );
+          appBar: AppBar(
+              title: context.watch<ReqresProvider>().isLoading
+                  ? CircularProgressIndicator(
+                      color: Theme.of(context).canvasColor,
+                    )
+                  : null),
+          body: Column(
+            children: [
+              Selector<ReqresProvider, bool>(builder: (context, value, child) {
+                return value ? const Placeholder() : const Text('data');
+              }, selector: (context, provider) {
+                return provider
+                    .isLoading; //isLoading durumunda çalışacak component oluşturmak için selector kullandık
+              }),
+              Expanded(
+                child: _resourceListView(
+                    context, context.watch<ReqresProvider>().resources),
+              ),
+            ],
+          ),
+        );
       },
       create: (context) => ReqresProvider(ReqrestService(service)),
     );
   }
+}
+
+ListView _resourceListView(BuildContext context, List<Data> items) {
+  return ListView.builder(
+      itemCount:
+          items.length, //context.watch<ReqresProvider>().resources.length
+      itemBuilder: (BuildContext context, int index) {
+        //inspect(resources[index].colorValue);
+        return Card(
+            color: Color(items[index].color?.colorValue ?? 0),
+            child: Text(items[index].name ?? ''));
+      });
 }
--- lib/advance_mid/reqrest_resource/viewModel/reqres_provider.dart
+++ lib/advance_mid/reqrest_resource/viewModel/reqres_provider.dart
@@ -6,12 +6,20 @@ import '../service/reqrest_service.dart';
 class ReqresProvider extends ChangeNotifier {
   final IReqrestService reqrestService;
   List<Data> resources = [];
+  bool isLoading = false;
+
+  void _changeLoading() {
+    isLoading = !isLoading;
+    notifyListeners();
+  }
 
   ReqresProvider(this.reqrestService) {
     _fetch();
   }
 
   Future<void> _fetch() async {
+    _changeLoading();
     resources = (await reqrestService.fetchResourceItem())?.data ?? [];
+    _changeLoading();
   }
 }
