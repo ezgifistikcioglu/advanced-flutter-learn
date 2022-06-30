80c7aa2cd047de5d47d913c5630a0e1b3de4aa74
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2022-06-30 13:37:08 +0300

    JsonSerializable, JsonAnotation, Build_runner

--- lib/advance_mid/reqrest_resource/model/resource_model.dart
+++ lib/advance_mid/reqrest_resource/model/resource_model.dart
@@ -1,56 +1,57 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'resource_model.g.dart';
+
+@JsonSerializable()
 class ResourceModel {
   List<Data>? data;
 
   ResourceModel({this.data});
 
-  ResourceModel.fromJson(Map<String, dynamic> json) {
-    if (json['data'] != null) {
-      data = <Data>[];
-      json['data'].forEach((v) {
-        data!.add(Data.fromJson(v));
-      });
-    }
+  factory ResourceModel.fromJson(Map<String, dynamic> json) {
+    //if (json['data'] != null) {
+    //  data = <Data>[];
+    //  json['data'].forEach((v) {
+    //    data!.add(Data.fromJson(v));
+    //  });
+    //}
+    return _$ResourceModelFromJson(json);
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    if (this.data != null) {
-      data['data'] = this.data!.map((v) => v.toJson()).toList();
-    }
-    return data;
+    // final Map<String, dynamic> data = <String, dynamic>{};
+    // if (this.data != null) {
+    //   data['data'] = this.data!.map((v) => v.toJson()).toList();
+    // }
+    // return data;
+    return _$ResourceModelToJson(this);
   }
 }
 
+// Eğer bu classdan bir servise istek atmayacaksak createToJson'ı false yapalım yer kaplamasın createToJson: false
+@JsonSerializable()
 class Data {
-  int? id;
-  String? name;
-  int? year;
-  String? color;
-  String? pantoneValue;
+  final int? id;
+  final String? name;
+  final int? year;
+  final String? color;
+  final String? pantoneValue;
+  final String? price;
 
-  Data({
-    this.id,
-    this.name,
-    this.year,
-    this.color,
-    this.pantoneValue,
-  });
+  Data(
+      {this.id,
+      this.name,
+      this.year,
+      this.color,
+      this.pantoneValue,
+      this.price});
 
-  Data.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    year = json['year'];
-    color = json['color'];
-    pantoneValue = json['pantone_value'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['name'] = name;
-    data['year'] = year;
-    data['color'] = color;
-    data['pantone_value'] = pantoneValue;
-    return data;
+  factory Data.fromJson(Map<String, dynamic> json) {
+    //id = json['id'];
+    //name = json['name'];
+    //year = json['year'];
+    //color = json['color'];
+    //pantoneValue = json['pantone_value'];
+    return _$DataFromJson(json);
   }
 }
